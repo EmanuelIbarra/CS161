adc3c05e25e0723b90894aea1b23d1b22195cde0
EmanuelIbarra <46576453+EmanuelIbarra@users.noreply.github.com>
2019-01-10 11:48:59 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//filename: ip2_variables_EmanuelI_MicahE_list.cpp
+// Description program to compute the length of a line that can be formed by a group of students holding hands
+/*
+    Author: Emanuel Ibarra
+    Partner: Micah Eckford
+    
+    Date: 1/10/2019
+    
+*/
+    
+    #include <iostream>
+    #include <iomanip> 
+    #include <string>
+    using namespace std;
+    
+    int main()
+    {
+        int numberStudents;
+        float armSpan;
+        float lineLength;
+        
+        
+        
+        numberStudents = 17;    
+        armSpan = 29.275;
+        lineLength = 497.675;
+    
+        cout << numberStudents << " students, with average armSpan of " << armSpan << " inches, " << endl;
+        cout << "can create a line that is " << lineLength << " inches long." << endl;
+        
+       
+       
+         return 0;
+    }
